d0783e6f61cf749eaf3e3b444e04b48f079865bf
Alena Dvorakova <alenadvorakova@technicky-preklad.cz>
2024-06-11 20:21:22 +0200

    Add function for Rodice, Studenti, Ucitele.

--- src/main/java/cz/czechitas/java2webapps/lekce10/service/StudentiService.java
+++ src/main/java/cz/czechitas/java2webapps/lekce10/service/StudentiService.java
@@ -1,7 +1,11 @@
 package cz.czechitas.java2webapps.lekce10.service;
 
+import cz.czechitas.java2webapps.lekce10.entity.Student;
+import cz.czechitas.java2webapps.lekce10.entity.Trida;
 import cz.czechitas.java2webapps.lekce10.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentiService {
@@ -10,4 +14,23 @@ public class StudentiService {
     public StudentiService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+    /**
+     * Vrací seznam všech studentů.
+     * @return Seznam studentů.
+     */
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    /**
+     * Vrací detail studenta.
+     * @param id Identifikátor studenta.
+     * @return Detail studenta nebo {@code null}.
+     */
+    public Student findById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+
 }
